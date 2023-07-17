8514df95818f10bb98dc442cc90a268bf16010e4
Christopher Moreau <christopher@join-jump.com>
2023-07-17 16:37:16 +0200

    remove log

--- webapp/src/components/DistributionChart.tsx
+++ webapp/src/components/DistributionChart.tsx
@@ -67,7 +67,6 @@ const DistributionChart: React.FC<DistributionChartProps> = ({ data }) => {
             mechanical: item.mechanical,
         }));
 
-        console.log(cleanData);
         const option: ECOption = {
             responsive: true,
             legend: {
